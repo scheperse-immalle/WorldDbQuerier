6a0bcf7b997ce23fcbedf4c36c29eae3120d0edf
Eline Schepers <eline.schepers@immalle.eu>
2017-09-28 09:20:08 +0200

    version  of coding excercise 1

--- WorldDbQuerier/Program.cs
+++ WorldDbQuerier/Program.cs
@@ -4,9 +4,29 @@ namespace WorldDbQuerier
 {
     class Program
     {
+        static string verionNumber = "0.1";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length > 0)
+            {
+                switch (args[0])
+                {
+                    case "-v":
+                        Console.WriteLine("versie {0}", verionNumber);
+                        break;
+                    default:
+                        Console.WriteLine("Onbekend argument");
+                        break;
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("???");
+            }
+
+            
         }
     }
 }
