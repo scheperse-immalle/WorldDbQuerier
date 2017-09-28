c2815ba8ec58affc0ae7801e2113b03f355c8f9d
Eline Schepers <eline.schepers@immalle.eu>
2017-09-28 09:47:47 +0200

    version of excercise 0.2

--- WorldDbQuerier/Program.cs
+++ WorldDbQuerier/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
 
 namespace WorldDbQuerier
 {
     class Program
     {
-        static string verionNumber = "0.1";
+        static string versionNumber = "0.1";
+        static string versionNumber1 = "0.2";
         static void Main(string[] args)
         {
             if (args.Length > 0)
@@ -12,7 +14,7 @@ namespace WorldDbQuerier
                 switch (args[0])
                 {
                     case "-v":
-                        Console.WriteLine("versie {0}", verionNumber);
+                        Console.WriteLine("versie {0}", versionNumber);
                         break;
                     default:
                         Console.WriteLine("Onbekend argument");
@@ -26,7 +28,24 @@ namespace WorldDbQuerier
                 Console.WriteLine("???");
             }
 
-            
+            MySqlConnection comm = new MySqlConnection();
+
+
+            comm.ConnectionString = "Server = 192.168.56.102;Port = 3306; Database = concerten;Uid = root;Pwd = r00t; ";
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = comm;
+            cmd.CommandText = "SELECT count(*) FROM world.Country";
+
+            comm.Open();
+
+            Console.WriteLine("Version {0} ; ", versionNumber1);
+            Console.WriteLine("Aantal landen {0}", cmd.ExecuteScalar());
+
+
+
+
+
         }
     }
 }
